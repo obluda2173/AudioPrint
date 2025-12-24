8a31bd51c6327197a37bb6a6b57e2881077370eb
obluda2173 <obluda2173@gmail.com>
2025-12-24 20:17:23 +0100

    work on fingerprint

--- src/fingerprint.jl
+++ src/fingerprint.jl
@@ -49,8 +49,47 @@ function find_peaks(spectrogram::Matrix{Float64}, amp_min::Float64=10.0, neighbo
     return peaks
 end
 
-function hash_peaks(peaks::Vector{Tuple{Int, Int}})
+function hash_peaks(peaks::Vector{Tuple{Int, Int}},   # basically peaks
+                    fan_out::Int = 10,                # max points to pair per anchor
+                    delta_t_min::Int = 0,             # min distance between points
+                    delta_t_max::Int = 200)           # max distance (target zone size))
 
+    sorted_peaks = sort(peaks, by = x -> x[1])
+
+    n_peaks = length(sorted_peaks)
+    hashes = Vector{NamedTuple{(:hash, :time_offset), Tuple{UInt32, Int}}}
+
+    sizehint!(hashes, n_peaks * fan_out)
+
+    for i in 1:n_peaks
+        anchor = sorted_peaks[i]
+        t1, f1 = anchor
+
+        start_idx = i + 1
+        end_idx = min(i + fan_out, n_peaks)
+
+        for j in start_idx:end_idx
+            target = sorted_peaks[j]
+            t2, f2 = target
+
+            delta_t = abs(t2 - t1)
+
+            if delta_t < delta_t_min || delta_t > delta_t_max
+                continue
+            end
+
+            # [f1 (9 bits)] | [f2 (9 bits)] | [delta_t (14 bits)]
+            f1_bits = UInt32(f1 & 0x1FF)       # 9 bits (0-511)
+            f2_bits = UInt32(f2 & 0x1FF)       # 9 bits (0-511)
+            dt_bits = UInt32(delta_t & 0x3FFF) # 14 bits (0-16383)
+
+            hash_val = (f1_bits << 23) | (f2_bits << 14) | dt_bits
+
+            push!(hashes, (hash = hash_val, time_offset = t1))
+        end
+    end
+
+    return hashes
 end
 
 end
