8a8bac08de4e4f592f93e96ef842eec93aae5540
obluda2173 <obluda2173@gmail.com>
2025-12-25 16:17:44 +0100

    new db table

--- src/fingerprint_db.jl
+++ src/fingerprint_db.jl
@@ -1,18 +1,31 @@
 using SQLite
 using DataFrames
 
-db = SQLite.DB("../db/shazam_clone.db")
+db = SQLite.DB("../db/songs.db")
 
 SQLite.execute(db, """
 DROP TABLE IF EXISTS Songs;
+DROP TABLE IF EXISTS Fingerprints;
 """)
 
+
 SQLite.execute(db, """
-CREATE TABLE IF NOT EXISTS Songs(
+CREATE TABLE IF NOT EXISTS Songs (
        song_id INTEGER PRIMARY KEY,
        title TEXT,
        artist TEXT
 );
 """)
+
+SQLite.execute(db, """
+CREATE TABLE IF NOT EXISTS Fingerprints (
+       hash INTEGER NOT NULL,
+       song_id INTEGER NOT NULL,
+       offset INTEGER NOT NULL,
+       FOREIGN KEY(song_id) REFERENCES Songs(song_id)
+)
+""")
+
+SQLite.execute(db, "CREATE INDEX IF NOT EXISTS idx_hash ON Fingerprints (hash)")
 
 SQLite.tables(db)
