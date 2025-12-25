635a63d5e8eada56a0d40b86d779202749113b28
obluda2173 <obluda2173@gmail.com>
2025-12-25 16:31:24 +0100

    organise database.jl

--- src/database.jl
+++ src/database.jl
@@ -1,28 +1,38 @@
 using SQLite
 using DataFrames
 
-db = SQLite.DB("../db/songs.db")
+function init_db(db_path)
+    db = SQLite.DB(db_path)
 
-# SQLite.execute(db, """
-# DROP TABLE IF EXISTS Songs;
-# DROP TABLE IF EXISTS Fingerprints;
-# """)
+    SQLite.execute(db, """
+    CREATE TABLE IF NOT EXISTS Songs (
+           song_id INTEGER PRIMARY KEY,
+           title TEXT,
+           artist TEXT
+    );
+    """)
 
-SQLite.execute(db, """
-CREATE TABLE IF NOT EXISTS Songs (
-       song_id INTEGER PRIMARY KEY,
-       title TEXT,
-       artist TEXT
-);
-""")
+    SQLite.execute(db, """
+    CREATE TABLE IF NOT EXISTS Fingerprints (
+           hash INTEGER NOT NULL,
+           song_id INTEGER NOT NULL,
+           offset INTEGER NOT NULL,
+           FOREIGN KEY(song_id) REFERENCES Songs(song_id)
+    )
+    """)
 
-SQLite.execute(db, """
-CREATE TABLE IF NOT EXISTS Fingerprints (
-       hash INTEGER NOT NULL,
-       song_id INTEGER NOT NULL,
-       offset INTEGER NOT NULL,
-       FOREIGN KEY(song_id) REFERENCES Songs(song_id)
-)
-""")
+    SQLite.execute(db, "CREATE INDEX IF NOT EXISTS idx_hash ON Fingerprints (hash)")
+end
 
-SQLite.execute(db, "CREATE INDEX IF NOT EXISTS idx_hash ON Fingerprints (hash)")
+function add_songs()
+
+end
+
+
+function add_fingerprint()
+
+end
+
+function query_fingerprints()
+
+end
